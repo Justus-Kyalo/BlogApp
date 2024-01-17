0368cb695bc06e0706dacc69b1fea0fea409758e
RemmyKyalo <chaloremmy@gmail.com>
2024-01-17 03:26:48 +0300

    styling navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
-
+import React from "react";
+import Logo from "./img/logo.png";
+import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <div className="Navbar">
+      <div className="container ">
+        <div className="logo">
+          <img src={Logo} alt="logo" />
+        </div>
+        <div className="links">
+          <Link to="/?cat=art" className="link">
+            <h6>ART</h6>
+          </Link>
+          <Link to="/?cat=scieche" className="link">
+            <h6>SCIENCE</h6>
+          </Link>
+          <Link to="/?cat=technology" className="link">
+            <h6>TECHNOLOGY</h6>
+          </Link>
+          <Link to="/?cat=cinema" className="link">
+            <h6>CINEMA</h6>
+          </Link>
+          <Link to="/?cat=design" className="link">
+            <h6>DESIGN</h6>
+          </Link>
+          <Link to="/?cat=food" className="link">
+            <h6>FOOD</h6>
+          </Link>
+          <span>John</span>
+          <span>LogOut</span>
+          <span className="write">
+            <Link className="link" to="/write">Write</Link>
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
