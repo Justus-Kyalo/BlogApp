ec13a7713a91da1c123c43ca33dcaabbfe3fe783
RemmyKyalo <chaloremmy@gmail.com>
2024-01-27 15:39:36 +0300

    connected the register page to the backend

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,15 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 const Register = () => {
+  const [inputs, setInputs] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+  const handleChange = (e) => {
+    setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try{
+      const res = await axios.post("http://localhost:8800/api/auth/register",inputs)
+
+      console.log(res);
+      
+    }catch(err){ 
+      console.log(err.message);
+    }
+  };
+
   return (
     <div className="auth">
       <h1>Register</h1>
-      <form action="">
-        <input required type="text" placeholder="username" />
-        <input required type="email" placeholder="email" />
-        <input required type="text" placeholder="password" />
-        <button>Register</button>
+      <form onSubmit={handleSubmit}>
+        <input
+          required
+          type="text"
+          placeholder="username"
+          name="username"
+          onChange={handleChange}
+        />
+        <input
+          required
+          type="email"
+          placeholder="email"
+          name="email"
+          onChange={handleChange}
+        />
+        <input
+          required
+          type="text"
+          placeholder="password"
+          name="password"
+          onChange={handleChange}
+        />
+        <button type="submit">Register</button>
         <div>
           <p>This is an error</p>
           <span>
