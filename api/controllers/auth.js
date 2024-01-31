812c7ca2be5862416b786ba82bb6140d2e55f4a1
RemmyKyalo <chaloremmy@gmail.com>
2024-01-31 21:12:26 +0300

    login controller

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,5 +1,6 @@
 import { db } from "../db.js";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 export const register = (req, res) => {
   //check esisting user
 
@@ -15,15 +16,46 @@ export const register = (req, res) => {
     const q = "INSERT INTO users (`username`,`email`,`password`) VALUES (?)";
     const values = [req.body.username, req.body.email, hash];
 
-    db.query(q, [values],(err, data ) => {
-      if(err) return res.json(err);
-      return res.status(200).json("user has been created"
-      );
-
+    db.query(q, [values], (err, data) => {
+      if (err) return res.json(err);
+      return res.status(200).json("user has been created");
     });
   });
 };
 
-export const login = (req, res) => {};
+export const login = (req, res) => {
+  const q = "SELECT * FROM users WHERE username= ? ";
+
+  db.query(q, [req.body.username], (err, data) => {
+    if (err) res.json(err);
+    if (!data.length) return res.status(404).json("User not found");
+
+    const hashedPwd = data[0].password;
+    const match = bcrypt.compareSync(req.body.password, hashedPwd);
+    if (!match) return res.status(404).json("User not found");
+    const token = jwt.sign({ id: data[0].id }, "jwtkey");
+    const { password, ...others } = data[0];
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(200)
+      .json(others);
+    // if (data.length) {
+    //   const hashedPwd = data[0].password;
+    //   const match = bcrypt.compareSync(req.body.password, hashedPwd);
+
+    //   const token = jwt.sign({ id: data[0].id }, "jwtkey");
+    //   const { password, ...others } = data[0];
+    //   res
+    //     .cookie("access_token", token, { httpOnly: true })
+    //     .status(200)
+    //     .json(others);
+    //   if (match) {
+    //     return res.status(200).json(data);
+    //   } else {
+    //     return res.status(400).json({ message: "Wrong username or password" });
+    //   }
+    // }
+  });
+};
 
 export const logout = (req, res) => {};
