88096708105d0f8810e6922ee5055d869c2ac067
RemmyKyalo <chaloremmy@gmail.com>
2024-02-13 09:19:22 +0300

    login and logout controllers set

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -6,7 +6,7 @@ export const register = (req, res) => {
 
   const q = "SELECT * FROM users WHERE email = ? OR username= ?";
   db.query(q, [req.body.email, req.body.username], (err, data) => {
-    if (err) return res.json(err);
+    if (err) return res.status(500).json(err);
     if (data.length) return res.status(409).json("User already exists");
 
     //Hash the password and create a user
@@ -17,29 +17,48 @@ export const register = (req, res) => {
     const values = [req.body.username, req.body.email, hash];
 
     db.query(q, [values], (err, data) => {
-      if (err) return res.json(err);
+      if (err) return res.status(500).json(err);
       return res.status(200).json("user has been created");
     });
   });
 };
 
 export const login = (req, res) => {
-  const q = "SELECT * FROM users WHERE username= ? ";
+  //CHECK USER
+
+  const q = "SELECT * FROM users WHERE username = ?";
 
   db.query(q, [req.body.username], (err, data) => {
-    if (err) res.json(err);
-    if (data.length === 0) return res.status(404).json("User not found");
+    if (err) return res.status(500).json(err);
+    if (data.length === 0) return res.status(404).json("User not found!");
 
-    const hashedPwd = data[0].password;
-    const match = bcrypt.compareSync(req.body.password, hashedPwd);
-    if (!match) return res.status(404).json("User not found");
+    //Check password
+    const isPasswordCorrect = bcrypt.compareSync(
+      req.body.password,
+      data[0].password
+    );
+
+    if (!isPasswordCorrect)
+      return res.status(400).json("Wrong username or password!");
+
     const token = jwt.sign({ id: data[0].id }, "jwtkey");
     const { password, ...other } = data[0];
+
     res
-      .cookie("access_token", token, { httpOnly: true })
+      .cookie("access_token", token, {
+        httpOnly: true,
+      })
       .status(200)
       .json(other);
   });
 };
 
-export const logout = (req, res) => {};
+export const logout = (req, res) => {
+  res
+    .clearCookie("access_token", {
+      sameSite: "none",
+      secure: true,
+    })
+    .status(200)
+    .json("User has been logged out");
+};
