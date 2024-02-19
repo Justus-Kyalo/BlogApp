00958aa23ba70856b416d34ae415ea2441c2be9c
RemmyKyalo <chaloremmy@gmail.com>
2024-02-19 10:57:47 +0300

    conditional rendering of edit & delete btn if post is for currentUser only to single post based on id got using useParams and sharing state on link to edit

--- client/src/pages/Single.jsx
+++ client/src/pages/Single.jsx
@@ -1,78 +1,66 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 import editIcon from "../img/edit.png";
 import deleteIcon from "../img/delete.png";
-import { Link } from "react-router-dom";
+import { Link, useLocation, useNavigate, useParams } from "react-router-dom";
 import Menu from "../components/Menu";
-
+import axios from "axios";
+import moment from "moment";
+import { AuthContext } from "../context/AuthContext";
 
 const Single = () => {
+  const { currentUser } = useContext(AuthContext);
+  const [post, setPost] = useState([]);
+
+  const navigate = useNavigate();
+
+  // const id = useLocation().pathname.split("/")[2];
+  const { id } = useParams(); //this hook is more simpler than the above in getting the id of the post from the url
+
+  const handleDelete = async () => {
+    try {
+      await axios.delete(`http://localhost:8800/api/posts/${id}`);
+      setPost(res.data);
+      navigate("/");
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await axios.get(`http://localhost:8800/api/posts/${id}`);
+        setPost(res.data);
+        console.log(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchData();
+  }, [id]);
   return (
     <div className="Single">
       <div className="content">
-        <img
-          src="https://images.pexels.com/photos/7008010/pexels-photo-7008010.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-          alt=""
-        />
+        <img src={post?.img} alt="" />
         <div className="user">
-          <img
-            src="https://images.pexels.com/photos/1040881/pexels-photo-1040881.jpeg?auto=compress&cs=tinysrgb&w=600"
-            alt=""
-          />
+          {post.userImg && <img src={post.userImg} alt="" />}
           <div className="info">
-            <span>John</span>
-            <p>Posted 2 days ago</p>
+            <span>{post?.username}</span>
+            <p>Posted {moment(post?.date).fromNow()}</p>
           </div>
-          <div className="edit">
-            <Link to={`/write?edit=2`}>
-              <img src={editIcon} alt="" />
-            </Link>
-            <img src={deleteIcon} alt="" />
-          </div>
+          {currentUser.username === post.username && (
+            <div className="edit">
+              <Link to={`/write?edit=${post.id}`} state={post}>
+                <img src={editIcon} alt="" />
+              </Link>
+              <img onClick={handleDelete} src={deleteIcon} alt="" />
+            </div>
+          )}
         </div>
-        <h1>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</h1>
-        <p>
-          Voluptatibus odit placeat iste, error accusamus laborum repudiandae a,
-          eius voluptatum magni quisquam nemo dolores. Molestiae delectus ad
-          excepturi reprehenderit non officiis. Lorem ipsum dolor sit amet.
-          Lorem ipsum dolor. Lorem ipsum dolor sit amet consectetur adipisicing
-          elit. Dolor laudantium deserunt dicta quae similique nemo, ipsa
-          ducimus delectus laboriosam eius fugit assumenda eligendi. Accusantium
-          natus aliquam maxime reiciendis sit culpa. Lorem ipsum, dolor sit amet
-          consectetur adipisicing elit. Quidem, voluptatem recusandae maxime
-          iste voluptates quibusdam consequuntur a nihil nemo maiores dolorem
-          veniam tenetur, natus aliquid assumenda enim deserunt dolores ducimus?
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum neque
-          dolores amet odit reiciendis eum, temporibus sunt modi, at tempora
-          deleniti quisquam veniam! Neque nulla, soluta quae recusandae adipisci
-          voluptate? ipsum dolor sit amet consectetur adipis. Lorem ipsum dolor
-          sit amet.
-          Lorem ipsum dolor. Lorem ipsum dolor sit amet consectetur adipisicing
-          elit. Dolor laudantium deserunt dicta quae similique nemo, ipsa
-          ducimus delectus laboriosam eius fugit assumenda eligendi. Accusantium
-          natus aliquam maxime reiciendis sit culpa. Lorem ipsum, dolor sit amet
-          consectetur adipisicing elit. Quidem, voluptatem recusandae maxime
-          iste voluptates quibusdam consequuntur a nihil nemo maiores dolorem
-          veniam tenetur, natus aliquid assumenda enim deserunt dolores ducimus?
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum neque
-          dolores amet odit reiciendis eum, temporibus sunt modi, at tempora
-          deleniti quisquam veniam! Neque nulla, soluta quae recusandae adipisci
-          voluptate? ipsum dolor sit amet consectetur adipis. Lorem ipsum dolor
-          sit amet.
-          Lorem ipsum dolor. Lorem ipsum dolor sit amet consectetur adipisicing
-          elit. Dolor laudantium deserunt dicta quae similique nemo, ipsa
-          ducimus delectus laboriosam eius fugit assumenda eligendi. Accusantium
-          natus aliquam maxime reiciendis sit culpa. Lorem ipsum, dolor sit amet
-          consectetur adipisicing elit. Quidem, voluptatem recusandae maxime
-          iste voluptates quibusdam consequuntur a nihil nemo maiores dolorem
-          veniam tenetur, natus aliquid assumenda enim deserunt dolores ducimus?
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum neque
-          dolores amet odit reiciendis eum, temporibus sunt modi, at tempora
-          deleniti quisquam veniam! Neque nulla, soluta quae recusandae adipisci
-          voluptate? ipsum dolor sit amet consectetur adipis. Lorem ipsum dolor
-          sit amet.
-        </p>
+        <h1>{post?.title}</h1>
+        <p>{post?.description}</p>
       </div>
-     <Menu/>
+      <Menu cat={post.cat} />
     </div>
   );
 };
