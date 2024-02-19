259f6ff76cf8b928cc86d3260f0e326aeb945696
RemmyKyalo <chaloremmy@gmail.com>
2024-02-19 10:15:11 +0300

    posts routes

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -1,8 +1,19 @@
 import express from "express";
+import {
+  addPost,
+  getPosts,
+  updatePost,
+  deletePost,
+  getPost,
+} from "../controllers/post.js";
 
 
 const router = express.Router();
 
-
+router.get("/", getPosts);
+router.get("/:id", getPost);
+router.post("/", addPost);
+router.delete("/:id", deletePost);
+router.put("/:id", updatePost);
 
 export default router;
