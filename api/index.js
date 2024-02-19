b1839eed850b88c18f53359034f23d93bb831a40
RemmyKyalo <chaloremmy@gmail.com>
2024-02-19 10:18:55 +0300

    added multer for image uploads

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import userRoutes from "./routes/users.js";
 import cors from "cors";
 import { corsOptions } from "./config/corsOptions.js";
 import cookieParser from "cookie-parser";
+import multer from "multer";
 
 const app = express();
 
@@ -13,6 +14,22 @@ app.use(cors(corsOptions));
 app.use(express.json());
 
 app.use(cookieParser());
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "../client/public/upload");
+  },
+  filename: function (req, file, cb) {
+        cb(null, Date.now() + file.originalname);
+  }, 
+});
+
+const upload = multer({ storage: storage });
+
+app.post("/api/upload", upload.single("file"), function (req, res) {
+  const file = req.file;
+  res.status(200).json(file.filename);
+});
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
