a1f0857aceedeb25e7c2964e7e6d794127802ed6
RemmyKyalo <chaloremmy@gmail.com>
2024-02-23 15:24:02 +0300

    foooter component

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -3,8 +3,10 @@ import Logo from "../img/logo.png";
 const Footer = () => {
   return (
     <footer>
+      <div className="footer-container">
       <img src={Logo} alt="" />
       <span>copyright &copy; 2024</span>
+      </div>
     </footer>
   );
 };
